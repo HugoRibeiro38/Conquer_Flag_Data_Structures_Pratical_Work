febe2c2c1015a23ce89b65fa5a56144a8132af47
HUGO MIGUEL GOMES ALVES RIBEIRO <ribeirohugo9400@gmail.com>
2023-01-28 17:15:06 +0000

    Created GlobalSettings.java which implements IGlobalSettings.java

--- src/main/java/API/GlobalSettings.java
+++ src/main/java/API/GlobalSettings.java
@@ -4,7 +4,6 @@ import API.Interfaces.IGlobalSettings;
 
 public class GlobalSettings implements IGlobalSettings {
 
-
     @Override
     public int calculateLevel(double experiencePoints, double x, double y) {
         return 0;
