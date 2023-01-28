7e6b865a294cb82f358cbce50136bd5ab734f449
HUGO MIGUEL GOMES ALVES RIBEIRO <ribeirohugo9400@gmail.com>
2023-01-28 17:06:59 +0000

    Created GlobalSettings.java + Interface

---
+++ src/main/java/API/GlobalSettings.java
@@ -0,0 +1,17 @@
+package API;
+
+import API.Interfaces.IGlobalSettings;
+
+public class GlobalSettings implements IGlobalSettings {
+
+
+    @Override
+    public int calculateLevel(double experiencePoints, double x, double y) {
+        return 0;
+    }
+
+    @Override
+    public int calculateExperiencePoints(int level) {
+        return 0;
+    }
+}
---
+++ src/main/java/API/Interfaces/IGlobalSettings.java
@@ -0,0 +1,8 @@
+package API.Interfaces;
+
+public interface IGlobalSettings {
+
+    int calculateLevel(double experiencePoints, double x, double y);
+
+    int calculateExperiencePoints(int level);
+}
