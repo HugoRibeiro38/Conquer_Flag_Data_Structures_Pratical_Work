d44af7af93a08bea56af42af6c4adf9a844fb105
HUGO MIGUEL GOMES ALVES RIBEIRO <ribeirohugo9400@gmail.com>
2023-01-31 23:26:23 +0000

    Developed tests for Portal.java, will be need finish method reload_energy from Connector.java to finish tests for Portal.java

--- src/test/java/Portal/PortalTest.java
+++ src/test/java/Portal/PortalTest.java
@@ -2,7 +2,9 @@ package Portal;
 
 import API.Enums.LocalType;
 import API.Enums.TeamType;
+import API.Exceptions.IllegalArgumentException;
 import API.Interfaces.IPortal;
+import API.Player;
 import API.Portal;
 import org.junit.jupiter.api.Test;
 
@@ -10,17 +12,54 @@ import static org.junit.jupiter.api.Assertions.*;
 
 public class PortalTest {
 
-    /**
-     *
-     */
     @Test
-    public void PortalTestT01() {
-        IPortal portal = new Portal("Felgueiras", 44.5f, 44.7f);
-        assertEquals(TeamType.NEUTRAL, portal.getStatus());
-        assertEquals(null, portal.getPlayer());
-        assertEquals("Felgueiras", portal.getName());
-        assertEquals(LocalType.PORTAL, portal.getType());
-        assertEquals(44.5f, portal.getLatitude());
-        assertEquals(44.7f, portal.getLongitude()); // XD
+    public void testConquerPortal() {
+        // Given
+        Player player = new Player("John", "Sparks");
+        Portal portal = new Portal("Portal 1", 0.0f, 0.0f);
+
+        // When
+        boolean result = portal.conquerPortal(player);
+
+        // Then
+        assertTrue(result);
+        assertEquals(0, player.getCurrentEnergy());
+        assertEquals(TeamType.SPARKS, portal.getStatus());
+    }
+
+
+    @Test
+    public void testConquerEnemyPortal() {
+        Player player = new Player("Hugo", "Giants");
+        Portal portal = new Portal("Portal 1", 0.0f, 0.0f);
+
+        boolean result = portal.conquerEnemyPortal(player);
+
+        assertTrue(result);
+        assertEquals(0, player.getCurrentEnergy());
+        assertEquals(TeamType.GIANTS, portal.getStatus());
+    }
+
+    @Test
+    public void addEnergyMoreThanThePlayerCan() {
+        Player player = new Player("Hugo", "Giants");
+        Portal portal = new Portal("Portal 1", 0.0f, 0.0f);
+
+        portal.conquerPortal(player);
+
+        assertThrows(IllegalArgumentException.class, () -> portal.addEnergy(player, 120));
+    }
+
+    @Test
+    public void addEnergyPlayerCanGive() {
+        Player player = new Player("Hugo", "Giants");
+        Portal portal = new Portal("Portal 1", 0.0f, 0.0f);
+
+        //First player need control of portal to add energy
+        portal.conquerPortal(player);
+
+        //Like he conquer portal he lost all energy, so he will need reload energy to add energy
+        assertEquals(0, player.getCurrentEnergy());
+        assertTrue(portal.addEnergy(player, 100));
     }
 }
