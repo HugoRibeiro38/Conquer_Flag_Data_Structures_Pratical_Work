a837ccdd03cd1ef0d3754cf1a0d00d97011c2c55
insosso <74792561+insosso@users.noreply.github.com>
2023-01-31 12:07:32 +0000

    Test for concreteGraph

--- src/main/java/Program/Main.java
+++ src/main/java/Program/Main.java
@@ -1,34 +1,48 @@
 package Program;
 
+import API.ConcreteGraph;
 import API.Connector;
-import API.Enums.LocalType;
 import API.Interfaces.IConnector;
 import API.Interfaces.IPortal;
-import API.Player;
 import API.Portal;
 
-import java.util.ArrayList;
-
 public class Main {
-    public static void main(String[] args) {
+  public static void main(String[] args) {
 
-        //Player player = new Player("Hugo", "Giants");
-        //System.out.println(player.getCurrentEnergy());
-
-
-        //Player player1 = new Player("João", "Sparks");
-
-        //IPortal portal = new Portal("F", 4, 4);
-
-        //System.out.println(portal.getID());
-
-       /* IPortal portal1 = new Portal("G", 5, 5);
+    //Player player = new Player("Hugo", "Giants");
+    //System.out.println(player.getCurrentEnergy());
 
 
-        System.out.println(portal1.getStatus());*/
-        IConnector connector = new Connector(1, 1);
-        System.out.println(connector.toString());
+    //Player player1 = new Player("João", "Sparks");
+
+    IPortal portal = new Portal("F", 4, 4);
+
+    System.out.println(portal.getID());
+
+    IPortal portal1 = new Portal("G", 5, 5);
 
 
-    }
+    System.out.println(portal1.getStatus());
+    IConnector connector = new Connector(1, 1);
+    System.out.println(connector.toString());
+
+    ConcreteGraph graph = new ConcreteGraph();
+
+
+    //System.out.println("ROUTE: "+graph.getRoutes().toString());
+    //System.out.println(route.size());
+    //graph.removeRoute(route);
+    //System.out.println("ROUTE: "+graph.getRoutes().toString());
+    graph.insertVertex(portal);
+    graph.insertVertex(portal1);
+    graph.insertVertex(connector);
+    graph.addRoute(1, 2);
+    graph.addRoute(3, 1);
+    graph.addRoute(2, 3);
+    graph.removeRoute(1, 3);
+    //System.out.println(graph.toString());
+    graph.removePlace(1);
+    System.out.println(graph.toString());
+
+  }
 }
