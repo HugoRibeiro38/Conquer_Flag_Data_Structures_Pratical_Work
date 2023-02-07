348ea7e031cef7c38affe7d40330ad7d10b9c2de
HUGO MIGUEL GOMES ALVES RIBEIRO <ribeirohugo9400@gmail.com>
2023-02-07 17:03:00 +0000

    Add documentation for ISimulatePlay.java

--- src/main/java/API/Interfaces/ISimulatePlay.java
+++ src/main/java/API/Interfaces/ISimulatePlay.java
@@ -1,7 +1,16 @@
 package API.Interfaces;
 
+/**
+ * Interface que representa a Simulação do jogo.
+ */
 public interface ISimulatePlay {
+    /**
+     * Incrementa o tempo da simulação.
+     */
     void incrementTime();
 
+    /**
+     * Inicia a simulação de jogo.
+     */
     void play();
 }
