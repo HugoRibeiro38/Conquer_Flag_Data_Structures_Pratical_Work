13add46ee1cb36637136d15d74b5bb2b60dd0d5d
HUGO MIGUEL GOMES ALVES RIBEIRO <ribeirohugo9400@gmail.com>
2023-02-07 17:27:26 +0000

    Add documentation for Portal.java

--- src/main/java/API/Portal.java
+++ src/main/java/API/Portal.java
@@ -9,15 +9,24 @@ import API.Interfaces.IPortal;
 
 import java.util.Scanner;
 
+/**
+ *  Classe que representa um objeto de Portal no jogo.
+ */
 public class Portal extends Local implements IPortal {
   private String name;
   private TeamType status;
-  // private Player OwnPlayer;
   private final static int DEFAULT_ENERGY = 0;
   private AGameSettings gameSettings;
 
-  private static final int MAX_ENERGY = 400; /* Visto que um connector tem energia de (100 - 300) */
+  private static final int MAX_ENERGY = 400;
 
+  /**
+   * Cria um novo portal com um nome, latitude e longitude definidos pelo user.
+   *
+   * @param name nome do portal
+   * @param latitude latitude do portal
+   * @param longitude longitude do portal
+   */
   public Portal(String name, float latitude, float longitude) {
     super(latitude, longitude);
     this.gameSettings = new GameSettings(DEFAULT_ENERGY, null);
@@ -26,28 +35,22 @@ public class Portal extends Local implements IPortal {
     this.status = TeamType.NEUTRAL;
   }
 
-  // Make method that allows a player to capture a portal with TeamType.Neutral,
-  // and if so the player needs to have atleast 25% of the maximum energy of the
-  // portal
 
+
+  /**
+   * Permite a um jogador conquistar um portal neutro.
+   *
+   * @param player player a tentar conquistar o portal
+   * @return true se o player conseguir conquistar o portal, caso contrário retorna false
+   */
   public boolean conquerPortal(IPlayer player) {
-    // TODO: Remove this if statement, and make it so that the player can capture a
-    // portal with TeamType.Neutral in the menu class
     if (this.status != TeamType.NEUTRAL) {
       return false;
     }
 
-    // Get the maximum energy of the portal
-    // TODO: Make missing method (getMaxEnergy)
     int portal_maxEnergy = this.getMAX_energy();
     int player_currentEnergy = player.getCurrentEnergy();
-    /*
-     * This checks if the energy is less or equal to current energy of the player
-     * and energy is greater or equal to 25% of the maximum energy of the portal
-     * if any part is false then, it returns ;
-     */
 
-    // TODO: Increment player score after capturing a Neutral portal
     if (player_currentEnergy >= (portal_maxEnergy * 0.25)) {
       player.setCurrentEnergy(player_currentEnergy - (int) (portal_maxEnergy * 0.25));
       // Energia do portal
@@ -65,54 +68,53 @@ public class Portal extends Local implements IPortal {
     return false;
   }
 
-  /*
-   * Dar uma vista de olhos no codigo para ver se esta tudo bem
+
+  /**
+   * Permite a um jogador tentar conquistar um portal inimigo.
+   *
+   * @param player o player a tentar conquistar um portal inimigo
+   * @return
    */
   public boolean conquerEnemyPortal(IPlayer player) {
-    // TODO: Remove this if statement, and make it so that the player can capture a
-    // portal with !=TeamType and TeamType!=NEUTRAL in the menu class
     if (this.status != player.getTeam() && this.status != TeamType.NEUTRAL) {
 
-      // GameSettings should be Max Energy of the Portal, not the Current Energy
       if (this.gameSettings.getEnergy() > player.getCurrentEnergy()) {
-        // TODO: Make custom Exception
         throw new IllegalArgumentException("Player does not have enough energy to capture this portal");
       }
 
-      // Energia da Torre 70
-      // Energia do player 70
 
-      // 0
       this.gameSettings.setEnergy(this.gameSettings.getEnergy() - player.getCurrentEnergy());
-      // 0
+
       int remainingEnergy = Math.abs(this.gameSettings.getEnergy());
 
-      // Check if the portal has no energy left
+
       if (gameSettings.getEnergy() < 0) {
         this.gameSettings.setEnergy(0);
       }
-      // 0
+
       player.setCurrentEnergy(remainingEnergy);
 
-      // TODO: Increment player score after capturing an Enemy portal
-      // OwnerShip to null
       GameSettings gameSettings = (GameSettings) this.gameSettings;
       gameSettings.ownerShip.player.setCapturedPortals(gameSettings.ownerShip.player.getCapturedPortals() - 1);
       gameSettings.getOwnerShip().setPlayer(null);
       this.status = TeamType.NEUTRAL;
       System.out.println("Player " + player.getName() + "de-captured the portal " + this.name);
       player.setExperiencePoints(player.getExperiencePoints() + GlobalSettings.captureEnemyPortal);
-      // Return true if Portal has been captured, false if not
 
     }
     return conquerPortal(player);
   }
 
-  // Make method that allows a player make a portal stronger by adding its current
-  // energy to the portal
+
+  /**
+   * Permite a um jogador adicionar energia a um portal que a sua equipa ja tenha conquistado.
+   *
+   * @param player player a querer adicionar energia ao portal
+   * @param energy energy a ser adicionada ao portal pelo player
+   * @return true se conseguir adicionar energy ao portal
+   */
   public boolean addEnergy(IPlayer player, int energy) {
 
-    // Not the player's team portal
     if (this.status != player.getTeam()) {
       throw new IllegalArgumentException("Portal does not belong to the player's team: " + player.getTeam());
     }
@@ -120,12 +122,10 @@ public class Portal extends Local implements IPortal {
     int player_currentEnergy = player.getCurrentEnergy();
     int portal_maxEnergy = this.getMAX_energy();
     int portal_currentEnergy = this.gameSettings.getEnergy();
-    // Check if the energy is more than the player has, if so throw an exception
     if (player_currentEnergy < energy) {
       throw new IllegalArgumentException("Energy is more than the player has");
     }
 
-    // CHANGES MADE IN THIS LINES
     int energyToAdd = Math.min(energy, portal_maxEnergy - portal_currentEnergy);
     this.gameSettings.setEnergy(this.gameSettings.getEnergy() + energyToAdd);
     player.setCurrentEnergy(player_currentEnergy - energyToAdd);
@@ -135,26 +135,50 @@ public class Portal extends Local implements IPortal {
     return true;
   }
 
-
+  /**
+   * Retorna o maximo de energia de um portal.
+   *
+   * @return MAX_ENERGY do portal
+   */
   public int getMAX_energy() {
     return Portal.MAX_ENERGY;
   }
 
+  /**
+   * Retorna o nome do portal.
+   *
+   * @return name do portal
+   */
   @Override
   public String getName() {
     return this.name;
   }
 
+  /**
+   * Define o nome do portal
+   *
+   * @param name o novo nome do portal.
+   */
   @Override
   public void setName(String name) {
     this.name = name;
   }
 
+  /**
+   * Retorna o tipo de local
+   *
+   * @return LocalType do local
+   */
   @Override
   public LocalType getType() {
     return super.localType;
   }
 
+  /**
+   * Menu para um jogador fazer iterações com as funções existentes dentro de um portal.
+   *
+   * @param player player a ser usado para trabalhar com as respetivas interações
+   */
   @Override
   public void menu(IPlayer player) {
     Scanner scanner = new Scanner(System.in);
@@ -184,47 +208,91 @@ public class Portal extends Local implements IPortal {
     }
   }
 
+  /**
+   * Retorna o estado do portal.
+   *
+   * @return status do portal
+   */
   @Override
   public TeamType getStatus() {
     return this.status;
   }
 
+  /**
+   * Retorna o quem conquistou(no caso de ter sido conquistado) o portal
+   *
+   * @return ownerShip do portal
+   */
   @Override
   public Player getPlayer() {
     GameSettings gameSettings1 = (GameSettings) this.gameSettings;
     return gameSettings1.ownerShip.getPlayer();
   }
 
-  // TODO: Refactor string Method
-  // GameSettings energy is the current energy of the portal
+
+  /**
+   * Inner class que representa um objeto de GameSettings de um Portal no jogo.
+   */
   public class GameSettings extends AGameSettings {
 
     private OwnerShip ownerShip;
 
+    /**
+     * Retorna quem conquistou o portal, no caso de ter sido conquistado.
+     *
+     * @return ownerShip do portal
+     */
     public OwnerShip getOwnerShip() {
       return ownerShip;
     }
 
+    /**
+     * Construtor de um GameSettings, com energy e player atribuidos automaticamente quando é instanciado um portal
+     *
+     * @param energy energia atual do portal
+     * @param player player que conquistou o portal
+     */
     public GameSettings(int energy, Player player) {
       super(energy);
       this.ownerShip = new OwnerShip(player);
     }
 
+    /**
+     * Inner class que representa um objeto de OwnerShip de um portal no jogo.
+     */
     private class OwnerShip {
       private Player player;
 
+      /**
+       * Construtor de um OwnerShip
+       * @param player player a ser atribuido ao ownership
+       */
       public OwnerShip(Player player) {
         this.player = player;
       }
 
+      /**
+       * Retorna o player do ownership.
+       * @return player conquistou portal.
+       */
       public Player getPlayer() {
         return player;
       }
 
+      /**
+       * Define o player do OwnerShip.
+       *
+       * @param player player que irá ser novo ownership do portal
+       */
       public void setPlayer(Player player) {
         this.player = player;
       }
 
+      /**
+       * Retorna o respetivo OwnerShip do portal
+       *
+       * @return player que conquistou o portal
+       */
       @Override
       public String toString() {
         return "OwnerShip{" +
